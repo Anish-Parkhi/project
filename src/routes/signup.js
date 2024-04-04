8496efa41cf0d266ece4f9ea860bd673e39b5148
Anish-Parkhi <anishparkhi03@gmail.com>
2024-04-05 03:02:41 +0530

    feat: added signup route

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,12 +1,29 @@
 import express from 'express';
-import bcrypt from 'bcrypt'
+import userModel from '../models/usersigninmodel';
 
 const Router = express.Router();
 
-Router.post('/user/signup', (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(404).json({ msg: 'Username or password not provided' });
-  }
+Router.post('/user/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(404).json({ msg: 'Username or password not provided' });
+    }
+    const userExists = userModel.findOne({ username });
+    if (userExists) {
+      return res.status(404).json({ msg: 'user already exists' });
+    }
+    const user = new userModel({
+      username: username,
+      password: password,
+    });
 
+    const userSavedRes = await user.save();
+    if (userSavedRes) {
+      res.status(200).json({ msg: 'User saved sucessfully' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
 });
