d7ebc2a681186e6324aeafabc87070dc7c6e4658
Anish-Parkhi <anishparkhi03@gmail.com>
2024-04-05 02:49:27 +0530

    feat: added the signup route

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,2 +1,12 @@
-import express from 'express'
+import express from 'express';
+import bcrypt from 'bcrypt'
 
+const Router = express.Router();
+
+Router.post('/user/signup', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(404).json({ msg: 'Username or password not provided' });
+  }
+
+});
