0a1b96fafff1db14bd7655a5cb1b27d3dba39640
Anish-Parkhi <anishparkhi03@gmail.com>
2024-04-06 01:45:41 +0530

    feat: Added cancel ticket route

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import cors from 'cors';
 import express from 'express';
 import db from './db.js';
-import signInRoute from './src/routes/signin.js';
-import signUpRouter from './src/routes/signup.js';
+import bookTicketRouter from './src/routes/Ticket/bookTicket.js';
+import cancelTicketRotuer from './src/routes/Ticket/cancelTicket.js';
 import allEventsRouter from './src/routes/event/allevents.js';
 import myEventsRouter from './src/routes/event/myevents.js';
-import bookTicketRouter from './src/routes/Ticket/bookTicket.js';
+import signInRoute from './src/routes/signin.js';
+import signUpRouter from './src/routes/signup.js';
 
 const app = express();
 
@@ -22,13 +23,12 @@ db.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
-
-
 app.use('/', signUpRouter);
 app.use('/', signInRoute);
-app.use('/event',allEventsRouter);
-app.use('/event',myEventsRouter)
-app.use('/ticket', bookTicketRouter)
+app.use('/event', allEventsRouter);
+app.use('/event', myEventsRouter);
+app.use('/ticket', bookTicketRouter);
+app.use('/ticket', cancelTicketRotuer);
 
 app.listen(3000, () => {
   console.log('App is listening on port 3000');
---
+++ src/routes/Ticket/cancelTicket.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import db from '../../../db.js';
+
+const cancelTicketRotuer = express();
+
+cancelTicketRotuer.delete('/cancelticket', (req, res) => {
+  try {
+    const { userId, ticketId } = req.body;
+    if (!userId || !ticketId) {
+      return res.json({ msg: 'paramters missing' });
+    }
+    db.query(
+      `delete from ticket where user_id = ? and ticket_id = ?`,
+      [userId, ticketId],
+      (err, result) => {
+        if (err) {
+          return res.status(500).json({ msg: 'Not able to cancel the ticket' });
+        } else {
+          db.query(
+            `delete from registrations where user_id = ? and ticket_id = ?`,
+            [userId, ticketId],
+            (err, result) => {
+              if (err) {
+                return res.status(500).json({ msg: 'Not able to unregister' });
+              } else {
+                return res.json({ msg: 'Ticket cancelled successfully' });
+              }
+            }
+          );
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
+});
+
+export default cancelTicketRotuer;
